8b2193e7386e9cc645392ebd2210fabf27621fe1
Gerardo <grrdnazar@gmail.com>
2024-01-06 23:34:17 -0600

    having trouble using privatekey in prod, trying a different way to parse then use the key.

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -4,12 +4,13 @@
 export const prerender = true;
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
+const { privateKey } = JSON.parse(import.meta.env.SERVER_FIREBASE_PRIVATE_KEY);
 const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.SERVER_FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.SERVER_FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.SERVER_FIREBASE_PRIVATE_KEY,
+  private_key: privateKey,
   client_email: import.meta.env.SERVER_FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.SERVER_FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.SERVER_FIREBASE_AUTH_URI,
